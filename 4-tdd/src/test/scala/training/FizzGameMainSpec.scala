5ac052e76bb184c4d6229dcf77a44a075bc5a5f7
Xi Qi <realxqi@gmail.com>
2015-07-02 01:36:51 +0800

    move FizzGameMainTest to FizzGameMainSpec to be ready for session 4
    Conflicts:
    	4-tdd/src/test/java/training/FizzGameMainTest.java

---
+++ 4-tdd/src/test/scala/training/FizzGameMainSpec.scala
@@ -0,0 +1,48 @@
+package training
+
+import java.io.{ByteArrayOutputStream, PrintStream}
+
+import org.scalatest.{Matchers, path}
+
+class FizzGameMainSpec extends path.FunSpec with Matchers {
+  describe("FizzGameMain") {
+    val outputStream: ByteArrayOutputStream = new ByteArrayOutputStream()
+    val originalOut: PrintStream = System.out
+    System.setOut(new PrintStream(outputStream))
+
+    describe("when input 3 5 7") {
+      FizzGameMain.main(Array("3", "5", "7"))
+      val result: Array[String] = outputStream.toString.split(System.lineSeparator())
+
+      it("students should say 100 lines") {
+        result.length shouldBe 100
+      }
+
+      it("student 1 should say 1") {
+        result(0) shouldBe "1"
+      }
+
+      it("student 3 should say Fizz") {
+        result(3 - 1) shouldBe "Fizz"
+      }
+
+      it("student 5 should say Buzz") {
+        result(5 - 1) shouldBe "Buzz"
+      }
+
+      it("student 7 should say Whizz") {
+        result(7 - 1) shouldBe "Whizz"
+      }
+
+      it("student 15 should say FizzBuzz") {
+        result(15 - 1) shouldBe "FizzBuzz"
+      }
+
+      it("student 21 should say FizzWhizz") {
+        result(21 - 1) shouldBe "FizzWhizz"
+      }
+    }
+
+    System.setOut(originalOut)
+  }
+}

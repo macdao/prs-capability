f53ca2f737156d4a0e2e2a9aeefe4e082dbb2ed2
Qi Xi <qixi@huawei.com>
2015-07-08 17:58:53 +0800

    write to file when the 4th arg present

--- 6-performance-1/src/test/java/training/FizzGameMainTest.java
+++ 6-performance-1/src/test/java/training/FizzGameMainTest.java
@@ -4,6 +4,9 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.contrib.java.lang.system.ExpectedSystemExit;
 import org.junit.contrib.java.lang.system.SystemErrRule;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+
+import java.nio.file.Files;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -11,6 +14,8 @@ import static org.junit.Assert.assertThat;
 public class FizzGameMainTest {
     @Rule
     public final SystemErrRule systemErrRule = new SystemErrRule().enableLog().mute();
+    @Rule
+    public final SystemOutRule systemOutRule = new SystemOutRule().enableLog().mute();
     @Rule
     public final ExpectedSystemExit exit = ExpectedSystemExit.none();
 
@@ -30,5 +35,14 @@ public class FizzGameMainTest {
         FizzGameMain.main(new String[]{"3", "5", "10"});
 
         assertThat(systemErrRule.getLog(), is("invalid input" + System.lineSeparator()));
+    }
+
+    @Test
+    public void should_write_to_file_when_input_3_5_7_file() throws Exception {
+        String file = Files.createTempFile("fizz-game-test", ".txt").toString();
+
+        FizzGameMain.main(new String[]{"3", "5", "7", file});
+
+        assertThat(systemOutRule.getLog(), is(""));
     }
 }

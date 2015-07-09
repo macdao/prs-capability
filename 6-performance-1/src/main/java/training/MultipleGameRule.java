72772052952ea98fff2b0797ca7b86c279462679
Qi Xi <qixi@huawei.com>
2015-07-09 17:17:14 +0800

    change multiple game rule to use map

--- 6-performance-1/src/main/java/training/MultipleGameRule.java
+++ 6-performance-1/src/main/java/training/MultipleGameRule.java
@@ -2,9 +2,15 @@ package training;
 
 import com.google.common.base.Optional;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MultipleGameRule implements GameRule {
 
     private GameInput gameInput;
+
+    public static final Map<String, Optional<String>> cache = Collections.synchronizedMap(new HashMap<String, Optional<String>>());
 
     public MultipleGameRule(GameInput gameInput) {
         this.gameInput = gameInput;
@@ -27,7 +33,15 @@ public class MultipleGameRule implements GameRule {
         }
 
         if (sb.length() != 0) {
-            return Optional.of(sb.toString());
+            String key = sb.toString();
+            final Optional<String> result;
+            if (!cache.containsKey(key)) {
+                result = Optional.of(key);
+                cache.put(key, result);
+            } else {
+                result = cache.get(key);
+            }
+            return result;
         }
 
         return Optional.absent();

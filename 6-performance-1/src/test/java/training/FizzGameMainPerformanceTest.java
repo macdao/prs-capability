5c76a699f533341cdeab4f6b33472f3291464de7
Xi Qi <realxqi@gmail.com>
2015-07-09 11:40:16 +0800

    add thread pool

--- 6-performance-1/src/test/java/training/FizzGameMainPerformanceTest.java
+++ 6-performance-1/src/test/java/training/FizzGameMainPerformanceTest.java
@@ -1,14 +1,30 @@
 package training;
 
 import java.io.File;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class FizzGameMainPerformanceTest {
 
     public static void main(String[] args) throws Exception {
-        for (int i = 0; i < 100 * 1000; i++) {
-            final File tempFile = File.createTempFile("fizz-game-test-", ".txt");
-            FizzGameMain.main(new String[]{"3", "5", "7", tempFile.getAbsolutePath()});
-            tempFile.delete();
+        final ExecutorService executorService = Executors.newFixedThreadPool(1);
+
+        final int count = 200 * 1000;
+        for (int i = 0; i < count; i++) {
+            executorService.submit(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        final File tempFile = File.createTempFile("fizz-game-test-", ".txt");
+                        FizzGameMain.main(new String[]{"3", "5", "7", tempFile.getAbsolutePath()});
+                        tempFile.delete();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
         }
+
+        executorService.shutdown();
     }
 }

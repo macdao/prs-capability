3b240188d1b4864602893111b1f372c78963410e
Xi Qi <realxqi@gmail.com>
2015-07-14 08:20:33 +0800

    add JMX throughput monitor

--- 7-performance-2/src/test/java/training/FizzGameMainPerformanceTest.java
+++ 7-performance-2/src/test/java/training/FizzGameMainPerformanceTest.java
@@ -1,23 +1,42 @@
 package training;
 
+import training.mbean.Training;
+
+import javax.management.*;
 import java.io.File;
+import java.lang.management.ManagementFactory;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class FizzGameMainPerformanceTest {
 
     private static final int count = 500 * 1000;
     private static final int threads = 8;
+    private static final Training training = new Training();
+    private static final AtomicInteger throughput = new AtomicInteger();
+    private static final AtomicLong throughputBegin = new AtomicLong();
 
     public static void main(String[] args) throws Exception {
+        registerMBean();
+
         warmUp();
 
         for (int i = 0; i < 3; i++) {
             final long start = System.currentTimeMillis();
             runMain(count, threads);
             System.out.println(System.currentTimeMillis() - start);
+        }
+
+        System.out.println(FizzGameMain.rules.size());
+    }
+
+    private static void registerMBean() throws MalformedObjectNameException, InstanceAlreadyExistsException, MBeanRegistrationException, NotCompliantMBeanException {
+        final ObjectName objectName = new ObjectName("training:type=Training");
+        if (!ManagementFactory.getPlatformMBeanServer().isRegistered(objectName)) {
+            ManagementFactory.getPlatformMBeanServer().registerMBean(training, objectName);
         }
     }
 
@@ -29,8 +48,10 @@ public class FizzGameMainPerformanceTest {
         final ExecutorService executorService = Executors.newFixedThreadPool(threads);
 
         final AtomicInteger finished = new AtomicInteger();
+        throughputBegin.set(System.currentTimeMillis());
         for (int i = 0; i < count; i++) {
-            if (i - finished.get() > 10 * 1000) {
+            training.setRunning(i - finished.get());
+            while (i - finished.get() > 10 * 1000) {
                 Thread.sleep(1000);
             }
             executorService.submit(new Runnable() {
@@ -43,6 +64,14 @@ public class FizzGameMainPerformanceTest {
                         if ((finished.incrementAndGet()) * 100 % count == 0) {
                             System.out.print("\r" + finished.get() * 100 / count + "%");
                         }
+
+                        throughput.incrementAndGet();
+                        final long now = System.currentTimeMillis();
+                        if (now - throughputBegin.get() > 1000) {
+                            throughputBegin.set(now);
+                            training.setThroughput(throughput.getAndSet(0));
+                        }
+
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
